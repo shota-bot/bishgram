629eb8559e5d5e1a162d63f18a2cca5d0cb53eb3
shota aoki <shotaaoki@mbp.local>
2021-01-27 15:32:32 +0900

    activehash導入

---
+++ app/models/member.rb
@@ -0,0 +1,11 @@
+class Member < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '___' },
+    { id: 2, name: 'アイナ・ジ・エンド' },
+    { id: 3, name: 'セントチヒロ・チッチ' },
+    { id: 4, name: 'モモコグミカンパニー' },
+    { id: 5, name: 'ハシヤスメ・アツコ' },
+    { id: 6, name: 'リンリン' },
+    { id: 7, name: 'アユニ・D' }
+  ]
+end
