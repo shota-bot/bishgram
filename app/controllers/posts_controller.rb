7abe900a9417584a0ecb3bdd703817085fb9d55a
shota aoki <shotaaoki@mbp.local>
2021-01-29 09:10:06 +0900

    showアクション、post.show作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :destroy]
 
   def index
     @posts = Post.includes(:user).order(created_at: :DESC)
@@ -19,9 +20,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
   private
 
   def post_params
     params.require(:posts_member).permit(:image, :text, { member_ids: [] }).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
