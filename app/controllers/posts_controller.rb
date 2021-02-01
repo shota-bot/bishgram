e4a4a964b43fce1947d8a57fb4d99b03ef482d48
shota aoki <shotaaoki@mbp.local>
2021-02-01 15:47:53 +0900

    gem 'bullet'インストール、indexのn+1解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:user).order(created_at: :DESC)
+    @posts = Post.with_attached_image.order(created_at: :DESC)
   end
 
   def new
