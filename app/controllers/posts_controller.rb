f90a99a45874927f829ecc9660c70191c42dbe33
shota aoki <shotaaoki@mbp.local>
2021-02-01 19:25:59 +0900

    検索機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,11 @@ class PostsController < ApplicationController
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.with_attached_image.order(created_at: :DESC)
+    if params[:member_id].present?
+      @posts = Member.find(params[:member_id]).posts.sort!{|a| a[:created_at]}
+    else
+      @posts = Post.with_attached_image.order(created_at: :DESC)
+    end
   end
 
   def new
