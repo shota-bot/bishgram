2fef47ccec6d0e60258b1081006df461161bf8ea
shota aoki <shotaaoki@mbp.local>
2021-02-02 14:46:39 +0900

    commentモデルのテストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :comment do
+    text { Faker::Lorem.characters(number: 10) }
+    association :user
+    association :post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿' do
+    context 'コメント投稿できる' do
+      it 'textが存在すれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメント投稿できない' do
+      it 'textがないと投稿できない' do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'userに紐づいていないと投稿できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("User must exist")
+      end
+      it 'postに紐づいていないと投稿できない' do
+        @comment.post = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Post must exist")
+      end
+      it '100文字以内でないと投稿できない' do
+        @comment.text = Faker::Lorem.characters(number: 101)
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text is too long (maximum is 100 characters)")
+      end
+    end
+  end
+
 end
