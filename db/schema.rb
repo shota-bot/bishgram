856a4bb53eb301dfe2098f71a82e597ebcde6380
shota aoki <shotaaoki@mbp.local>
2021-02-03 15:43:22 +0900

    relationモデルテスト

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,9 @@ ActiveRecord::Schema.define(version: 2021_02_03_051214) do
     t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user_1 = FactoryBot.create(:user)
+    @user_2 = FactoryBot.create(:user)
+    @relationship = Relationship.new(follower_id: @user_1.id, followed_id: @user_2.id)
+  end
+
+  describe "フォロー登録" do
+    context "フォローできる" do
+      it "follower_id, followed_idがあれば登録できる" do
+        expect(@relationship).to be_valid
+      end
+    end
+    context "フォローできない" do
+      it "follower_idが存在しないと登録できない" do
+        @relationship.follower_id = nil
+        @relationship.valid?
+        expect(@relationship.errors.full_messages).to include("Followerを入力してください")
+      end
+      it "followed_idが存在しないと登録できない" do
+        @relationship.followed_id = nil
+        @relationship.valid?
+        expect(@relationship.errors.full_messages).to include("Followedを入力してください")
+      end
+      it "同じユーザーを登録できない" do
+        @relationship.save
+        another_relationship = Relationship.new(follower_id: @user_1.id, followed_id: @user_2.id)
+        another_relationship.valid?
+        expect(another_relationship.errors.full_messages).to include("Followerはすでに存在します")
+      end
+    end
+  end
 end
