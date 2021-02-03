05030638217976e2c14cd9a79e19eaff3a830615
shota aoki <shotaaoki@mbp.local>
2021-02-03 16:33:37 +0900

    フォロー、フォロワー一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = Post.with_attached_image
-    @posts = @posts.where(user_id: @user.id).order(created_at: :DESC)
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_list = Post.with_attached_image.find(favorites).sort! { |a| a[:created_at] }
-    @followers = Relationship.where(follower_id: @user.id)
-    @followeds = Relationship.where(followed_id: @user.id)
   end
 end
