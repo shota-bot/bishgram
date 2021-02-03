70861f92c1472802d64918cd3690cbfc240a939e
shota aoki <shotaaoki@mbp.local>
2021-02-03 15:00:25 +0900

    フォロー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
     @posts = @posts.where(user_id: @user.id).order(created_at: :DESC)
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
     @favorite_list = Post.with_attached_image.find(favorites).sort! { |a| a[:created_at] }
+    @followers = Relationship.where(follower_id: @user.id)
+    @followeds = Relationship.where(followed_id: @user.id )
   end
 end
