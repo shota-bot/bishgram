82f750b629be1816f95cbbc6a5dee0cad7e3a38f
shota aoki <shotaaoki@mbp.local>
2021-02-11 18:44:09 +0900

    新規投稿の画像プレビュー

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,24 @@
 if(location.pathname.match("posts/new")|| location.pathname.match("posts/edit")){
+  document.addEventListener("DOMContentLoaded", function(){
+    const ImgPrev = document.getElementById('img-prev');
 
+    document.getElementById('posts_member_image').addEventListener('change', function(e){
+      //画像データを取得
+      const file = e.target.files[0];
+      // 画像データから画像URLを作成
+      const blob = window.URL.createObjectURL(file);
+
+      //画像表示のためのdiv生成
+      const imageElement = document.createElement('div');
+
+      // img要素生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', 'w-100');
+      
+      // 親要素に子要素を追加
+      imageElement.appendChild(blobImage);
+      ImgPrev.appendChild(imageElement);
+    });
+  });
 };
